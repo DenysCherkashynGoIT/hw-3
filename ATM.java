568f2112e79284401a746a675875d7cb20951093
Den Cherkashyn <d.a.cherkashin@ukr.net>
2020-12-20 00:11:26 +0200

    My first commit

---
+++ ATM.java
@@ -0,0 +1,17 @@
+package main.java;
+
+public class ATM {
+    public int countBanknotes (int sum) {
+        int quantity = 0;
+        int[] nominals = new int[] {500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int i=0;
+        while (sum>0) {
+            if (sum >= nominals[i]) {
+                quantity += sum/nominals[i];
+                sum = sum%nominals[i];
+            }
+            i++;
+        }
+        return quantity;
+    }
+}
